b23638dd84f7ad475443c7e895b2bb4d16ed0dd5
ArtemPavl <artem.pavlenko.93@inbox.ru>
2022-10-08 18:46:59 +0300

    Homework2

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание к уроку 7");
         //Домашнее задание 1
-        System.out.println("Домашнее задание 1");
+        System.out.println("Домашнее задание 2");
         //Задание 1
         System.out.println("Задание 1");
-        for(int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        for(int i = 1904; i <= 2096; i=i+4) {
+            System.out.println(i + " год является висакосным");
         }
         //Задание 2
         System.out.println("Задание 2");
-        for(int i = 10; i >= 1; i=i-1) {
+        for(int i = 7; i <= 98; i=i+7) {
              System.out.println(i);
         }
         //задание 3
         System.out.println("Задание 3");
-        for(int i = 2; i < 17; i = i +2){
+        for(int i = 1; i <= 512; i = i*2){
             System.out.println(i);
         }
-        //Задание 4
-        System.out.println("Задание 4");
-        for(int i = 10; i >= -10; i=i-1) {
-            System.out.println(i);
-        }
-        }
-
     }
+
+}
