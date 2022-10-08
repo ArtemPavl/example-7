fd3ef483cd716cf666727c0e26e931478511c5e6
ArtemPavl <artem.pavlenko.93@inbox.ru>
2022-10-08 19:02:02 +0300

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,26 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание к уроку 7");
         //Домашнее задание 1
-        System.out.println("Домашнее задание 2");
+        System.out.println("Домашнее задание 3");
         //Задание 1
         System.out.println("Задание 1");
-        for(int i = 1904; i <= 2096; i=i+4) {
-            System.out.println(i + " год является висакосным");
+        int contribution = 29000;
+        int total = 0;
+        for(int i = 1; i <= 12; i++) {
+            total = total + contribution;
+            System.out.println("Месяц " + i + ", сумма накоплений равна "
+                    + total + " рублей");
         }
         //Задание 2
         System.out.println("Задание 2");
-        for(int i = 7; i <= 98; i=i+7) {
-             System.out.println(i);
-        }
-        //задание 3
-        System.out.println("Задание 3");
-        for(int i = 1; i <= 512; i = i*2){
-            System.out.println(i);
+        contribution = 29000;
+        total = 0;
+        int procent = 12;
+        for(int i = 1; i <= 12; i++) {
+            total = total + total*procent/100;
+            total = total + contribution;
+            System.out.println("Месяц " + i + ", сумма накоплений равна "
+                    + total + " рублей");
         }
     }
 
