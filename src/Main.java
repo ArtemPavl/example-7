33c6e729c96c4999a35c798cbe2e5df66cf6dffb
ArtemPavl <artem.pavlenko.93@inbox.ru>
2022-10-08 18:32:49 +0300

    Homework1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание к уроку 7");
+        //Домашнее задание 1
+        System.out.println("Домашнее задание 1");
+        //Задание 1
+        System.out.println("Задание 1");
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        //Задание 2
+        System.out.println("Задание 2");
+        for(int i = 10; i >= 1; i=i-1) {
+             System.out.println(i);
+        }
+        //задание 3
+        System.out.println("Задание 3");
+        for(int i = 2; i < 17; i = i +2){
+            System.out.println(i);
+        }
+        //Задание 4
+        System.out.println("Задание 4");
+        for(int i = 10; i >= -10; i=i-1) {
+            System.out.println(i);
+        }
+        }
+
     }
-}
